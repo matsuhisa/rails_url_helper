488772098ebb34a8665f10d63662d634bf1ebb98
matsuhisa <matsuhisa+github@gmail.com>
2016-01-11 01:22:45 +0900

    Refactoring menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def request_path_include?(string)
+    request.path.include?(string.to_s)
+  end
+
+  def link_to_if_with_block(condition, options = nil, html_options = nil, &block)
+    if condition
+      link_to(options, html_options, &block)
+    else
+      capture(&block)
+    end
+  end
+
 end
