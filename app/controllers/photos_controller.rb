c219711f7bbc4a586b371b98b436cb5da64f59ba
matsuhisa <matsuhisa+github@gmail.com>
2016-01-10 17:31:55 +0900

    Setting shop/:shop_id/photos, shop/:shop_id/photos/:id

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,28 +1,21 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_shop, only: [:index, :show]
 
-  # GET /photos
-  # GET /photos.json
   def index
-    @photos = Photo.all
+    @photos = @shop.photos
   end
 
-  # GET /photos/1
-  # GET /photos/1.json
   def show
   end
 
-  # GET /photos/new
   def new
     @photo = Photo.new
   end
 
-  # GET /photos/1/edit
   def edit
   end
 
-  # POST /photos
-  # POST /photos.json
   def create
     @photo = Photo.new(photo_params)
 
@@ -37,8 +30,6 @@ class PhotosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /photos/1
-  # PATCH/PUT /photos/1.json
   def update
     respond_to do |format|
       if @photo.update(photo_params)
@@ -51,8 +42,6 @@ class PhotosController < ApplicationController
     end
   end
 
-  # DELETE /photos/1
-  # DELETE /photos/1.json
   def destroy
     @photo.destroy
     respond_to do |format|
@@ -62,12 +51,14 @@ class PhotosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    def set_shop
+      @shop = Shop.find(params[:shop_id])
+    end
+
     def set_photo
       @photo = Photo.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
       params.require(:photo).permit(:title, :description)
     end
